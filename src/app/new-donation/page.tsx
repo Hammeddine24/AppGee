116c76ec9bf996bffdd44c2582286be5854088e8
Studexus Group <studexusgroup@gmail.com>
2025-08-18 14:05:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/new-donation/page.tsx
+++ src/app/new-donation/page.tsx
@@ -16,6 +16,7 @@ import { Textarea } from '@/components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Progress } from "@/components/ui/progress";
+import { Label } from "@/components/ui/label";
 import Image from 'next/image';
 import { useToast } from "@/hooks/use-toast";
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
