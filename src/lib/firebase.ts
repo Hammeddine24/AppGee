ff25b8416fc676d12adb19c793e41657b375dd9b
Studexus Group <studexusgroup@gmail.com>
2025-08-18 12:51:30 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp, getApps } from "firebase/app";
+
+const firebaseConfig = {
+  "projectId": "re-life-hub",
+  "appId": "1:509031975989:web:c1935389dfe5b858bdb57c",
+  "storageBucket": "re-life-hub.firebasestorage.app",
+  "apiKey": "AIzaSyA-v7ZjqR3GPq7RdM6ELE5LDAzILMjQFzU",
+  "authDomain": "re-life-hub.firebaseapp.com",
+  "messagingSenderId": "509031975989"
+};
+
+// Initialize Firebase
+let firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export default firebaseApp;
