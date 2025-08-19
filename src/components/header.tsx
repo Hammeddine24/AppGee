0e14f2827fed004ee6379516ca1103d4956624a5
Studexus Group <studexusgroup@gmail.com>
2025-08-19 11:20:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,23 +6,21 @@ import { UserNav } from "@/components/user-nav";
 export function Header() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+      <div className="container flex h-16 items-center justify-between px-4">
         <div className="flex gap-6 md:gap-10">
           <a href="/home" className="flex items-center space-x-2">
             <Leaf className="h-6 w-6 text-primary" />
             <span className="inline-block font-bold text-lg">Gee</span>
           </a>
         </div>
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-2">
-            <Button asChild>
-              <Link href="/new-donation">
-                <PlusCircle className="mr-2 h-4 w-4" />
-                Nouveau don
-              </Link>
-            </Button>
-            <UserNav />
-          </nav>
+        <div className="flex items-center space-x-2">
+          <Button asChild>
+            <Link href="/new-donation">
+              <PlusCircle className="mr-2 h-4 w-4" />
+              Nouveau don
+            </Link>
+          </Button>
+          <UserNav />
         </div>
       </div>
     </header>
